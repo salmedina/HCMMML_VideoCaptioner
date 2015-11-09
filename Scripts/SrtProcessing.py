02411cbadcbd878e30858cd1f08cc103700f7497
Salvador Medina <salvadom@andrew.cmu.edu>
2015-11-09 00:29:06 -0500

    ADD SRT info extractor, ADD Images folder which contains images for midterm report

--- Scripts/SrtProcessing.py
+++ Scripts/SrtProcessing.py
@@ -4,13 +4,31 @@ import collections
 MovieInfo = collections.namedtuple('MovieInfo', 'filename times caption')
 
 def get_video_clips(srtFileName):
-    pass
+    fileList = []
+    if os.path.isfile(srtFileName):
+        srtLines = open(srtFileName,'r').readlines()
+        fileList = [srtLines[i] for i in range(0,len(srtLines),4)]
+    
+    return fileList
 
 def get_times(srtFileName):
-    pass
+    timesList = []
+    if os.path.isfile(srtFileName):
+        srtLines = open(srtFileName,'r').readlines()
+        timesList = [srtLines[i] for i in range(1,len(srtLines),4)]
+    
+    return timesList
 
 def get_captions(srtFileName):
-    pass
+    captionSentences = []
+    if os.path.isfile(srtFileName):
+        srtLines = open(srtFileName,'r').readlines()
+        captionSentences = [srtLines[i] for i in range(2,len(srtLines),4)]
+    
+    return captionSentences
 
 def get_movie_info(srtFileName):
-    
+    fileList = get_video_clips(srtFileName)
+    durationList = get_durations(srtFileName)
+    captionsList = get_captions(srtFileName)
+    return zip(fileList, durationList, captionsList)
